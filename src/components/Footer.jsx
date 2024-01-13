e3a2357a50fcbc0b673955a79aaee039a29099f8
Roman Moiseenko <humidityisrising@gmail.com>
2024-01-13 18:21:40 +0200

    add lab5 and lab6

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import './../styles/footer.css'
+
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <div className="footer_contact-info">
+                <h3>Контакты</h3>
+                <p>Телефон: +1556541451</p>
+                <p>Адрес: ул. Ленина, 45</p>
+                <p>Email: registratura@gmail.com</p>
+            </div>
+    </footer>
+     );
+}
+ 
+export default Footer;
